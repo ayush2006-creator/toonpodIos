5a6261ed6ec91bb3c57a6c1a20e7b5c43be9f682
pixelforge <poddaryush52@gmail.com>
2026-03-31 15:20:05 +0530

    Initial Commit

---
+++ Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by ayush poddar on 31/03/26.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
